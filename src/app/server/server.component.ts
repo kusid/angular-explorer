4006c0fff2cd59340afe4ebb9b331bf5dc3ed90e
kumar siddhartha <kumar.siddhartha@hotmail.com>
2017-06-06 23:58:11 -0400

    two way binding simple exercise

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,12 +2,17 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: 'server-component',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
 
   serverId: number = 100;
   serverStatus: string = 'offline';
+
+  serverCreationStatus: string = "No server was created!";
+
+  username: string = '';
 
   getServerStatus(): string {
     return this.serverStatus;
@@ -16,8 +21,20 @@ export class ServerComponent {
   disabled: boolean = false;
 
   constructor() {
-    setTimeout( () => {
+    setTimeout(() => {
       this.disabled = true;
     }, 3000)
   }
+
+  createServer() {
+    this.serverCreationStatus = "Server was created by " + this.username;
+  }
+
+  resetServer() {
+    if (this.username.length != 0) {
+      this.username = '';
+    }
+  }
+
+
 }
